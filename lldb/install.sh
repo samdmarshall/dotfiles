0ca35580adf99b5e2be244be055896d600721400
samdmarshall <me@samdmarshall.com>
2015-01-17 12:02:40 -0500

    adding code to pull down scripts

--- lldb/install.sh
+++ lldb/install.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
+curr=`pwd`
+mkdir -p ~/Projects
+cd ~/Projects
+
+if which git > /dev/null; then
+	# setting up dbscript
+	git clone git@github.com:samdmarshall/lldbscript.git lldbscript
+	# setting up voltron
+	git clone git@github.com:snare/voltron.git voltron
+	cd voltron
+	python setup.py install
+else
+	echo "please install git first!"
+fi
+
+cd DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cp lldbinit ~/.lldbinit
+
+cd $curr
