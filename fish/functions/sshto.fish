a1fb60a657080e1bf4ef6e80b3469073173732be
samdmarshall <me@samdmarshall.com>
2015-01-16 20:52:39 -0500

    removing case-sensitive hostnames from sshto

--- fish/functions/sshto.fish
+++ fish/functions/sshto.fish
@@ -3,11 +3,11 @@ function sshto
 	if [ "$argv" != $empty_string ];
 		set host_connect "$argv[1]"
 		
-		if [ "$host_connect" = "Pegasus" ];
+		if test "$host_connect" = "Pegasus" -o "$host_connect" = "pegasus"
 			ssh sam@Pegasus.local
-		else if [ "$host_connect" = "Galactica" ];
+		else if test "$host_connect" = "Galactica" -o "$host_connect" = "galactica" 
 			ssh sam@Galactica.local
-		else if [ "$host_connect" = "Linode" ];
+		else if test "$host_connect" = "Linode" -o "$host_connect" = "linode"
 			ssh samdm@samdmarshall.com
 		else
 			echo "invalid host!"
