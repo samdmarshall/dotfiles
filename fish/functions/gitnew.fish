7ffa2c57400aa5ea9282b3a7a34af291f277b133
samdmarshall <me@samdmarshall.com>
2015-02-15 18:10:22 -0500

    adding defaults for contributing and license

--- fish/functions/gitnew.fish
+++ fish/functions/gitnew.fish
@@ -4,5 +4,6 @@ function gitnew
 	cp ~/.config/defaults/clang-format ./.clang-format
 	cp ~/.config/defaults/gitignore ./.gitignore
 	cp ~/.config/defaults/LICENSE ./LICENSE
+	cp ~/.config/defaults/CONTRIBUTING.md ./CONTRIBUTING.md
 	
 end
