1e4e0e84f491a74b7c11489eb5a3ef6c343e2bb8
samdmarshall <me@samdmarshall.com>
2015-07-18 13:54:33 -0400

    upload

---
+++ fish/functions/dot.fish
@@ -0,0 +1,32 @@
+function dot --description 'dot file management' --argument dot_command
+
+	set arg_count (echo $argv | wc -w | awk '{print $1}')
+
+	if [ $arg_count -eq 1 ];
+		set CURR (pwd)
+		cd $HOME_CONFIG_PATH
+		
+		if test "$dot_command" = "upload"
+			git add -f fish/config.fish fish/functions/*.fish defaults/* fish/functions/gensite/*
+			if test -e $HOME_CONFIG_PATH/fish/functions/testfunc.fish
+				git reset -- fish/functions/testfunc.fish 
+			end
+			git commit -m "$argv"
+			git push origin master
+			set dot_command "reload"
+		end
+
+		if test "$dot_command" = "update"
+			git pull
+			set dot_command "reload"
+		end
+
+		if test "$dot_command" = "reload"
+			reload
+		end
+		
+		cd $CURR
+	else
+		echo "usage: dot [upload|update|reload]"
+	end
+end
