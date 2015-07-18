77c9db4e20b50380f18dbe893b10913aa4da4136
samdmarshall <me@samdmarshall.com>
2015-07-18 16:52:59 -0400

    💻

--- fish/functions/fixmenubar.fish
+++ fish/functions/fixmenubar.fish
@@ -2,6 +2,10 @@ function fixmenubar
 	sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
 	sudo chmod 600 /System/Library/CoreServices/Spotlight.app/Contents/MacOS/Spotlight
 	sudo chmod 600 /System/Library/CoreServices/NotificationCenter.app/Contents/MacOS/NotificationCenter
+	
+	launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
+	sudo defaults write /System/Library/LaunchAgents/com.apple.notificationcenterui KeepAlive -bool true
+	
 	killall NotificationCenter
 	killall SystemUIServer
 	killall Spotlight
