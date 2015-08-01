09a002c5c3254f817163d5499ec016a65bee7436
samdmarshall <me@samdmarshall.com>
2015-08-01 09:56:37 -0400

    💻

--- fish/paths.fish
+++ fish/paths.fish
@@ -11,8 +11,12 @@ set LOCAL_RUBY_PATH ""
 if test -e  ~/.gem/ruby/2.0.0/bin
 	set LOCAL_RUBY_PATH ~/.gem/ruby/2.0.0/bin
 end
+set TEXBIN_PATH ""
+if test -e /usr/texbin
+	set TEXBIN_PATH /usr/texbin
+end
 # setting $PATH
-set PATH $PATH $LOCAL_PYTHON_PATH $LOCAL_RUBY_PATH $XCODE_NODE_PATH
+set PATH $PATH $LOCAL_PYTHON_PATH $LOCAL_RUBY_PATH $XCODE_NODE_PATH $TEXBIN_PATH
 
 # setting $GEM_HOME
 set LOCAL_GEM_HOME ""
