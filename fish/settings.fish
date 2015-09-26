4938991be5b0f42551fa0a1616454ff31c667fea
Samantha Marshall <me@samdmarshall.com>
2015-09-26 13:08:18 -0400

    💻

--- fish/settings.fish
+++ fish/settings.fish
@@ -8,14 +8,16 @@ set -x GIT_DEFAULTS_DIR $HOME_CONFIG_PATH/defaults
 
 set -x CORE_SCRIPTS_PATH $HOME_CONFIG_PATH/scripts
 
-set -xg KEY_STORAGE_PATH $HOME_CONFIG_PATH/storage/keys.keychain
+set -xg KEY_STORAGE_KEYCHAIN_NAME keys.keychain
+
+set -xg KEY_STORAGE_PATH $HOME_CONFIG_PATH/storage/$KEY_STORAGE_KEYCHAIN_NAME
 
 set -x HOMEBREW_INSTALL_BADGE 🌈
 
 set -x PLATFORM_NAME (uname -s)
 
 function secure_note_storage --argument name
-	set keys_keychain_password (security find-generic-password -l keys.keychain -w)
+	set keys_keychain_password (security find-generic-password -l $KEY_STORAGE_KEYCHAIN_NAME -w)
 	security unlock-keychain -p $keys_keychain_password $KEY_STORAGE_PATH
 	set note_contents (security find-generic-password -C note -w -l $name $KEY_STORAGE_PATH | xxd -r -p | plutil -p - | grep "NOTE" | awk '{gsub(/"/, "", $3); print $3}')
 	echo $note_contents
@@ -23,9 +25,12 @@ end
 
 if which security > /dev/null
 
-	# this will need to be updated when it changes, connect to server and netstat for bouncer connection
-	set -g HOME_IP (secure_note_storage HOME_IP)
+	set found_registered_keychain (security list-keychains | grep "$KEY_STORAGE_KEYCHAIN_NAME" | wc -l)
+	if [ $found_registered_keychain -eq 1 ];
+		# this will need to be updated when it changes, connect to server and netstat for bouncer connection
+		set -g HOME_IP (secure_note_storage HOME_IP)
 
-	set -x HOMEBREW_GITHUB_API_TOKEN (secure_note_storage HOMEBREW_GITHUB_API_TOKEN)
+		set -x HOMEBREW_GITHUB_API_TOKEN (secure_note_storage HOMEBREW_GITHUB_API_TOKEN)
+	end
 
 end
