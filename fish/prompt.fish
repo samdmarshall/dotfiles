188d3cb6c0c564cb03ee3067f1ac2c1c4c6d12c2
Samantha Marshall <me@samdmarshall.com>
2015-09-28 12:18:33 -0400

    💻

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -146,21 +146,30 @@ function source_control_prompt
 end
 
 function prompt_current_working_dir
+	set should_show_path true
 	set path_prefix ""
 	set working_path (pwd | sed -e "s=^$HOME=~=")
+	set default_length "40"
 	set total_length "38"
 	if [ $HAS_WC = true ];
 		#setting up current working dir for path truncation
 		set user_length (echo -n $USER | wc -c)
 		set host_length (echo -n $__fish_prompt_hostname | wc -c)
 		set working_path_length (echo -n $working_path | wc -c)
-		set total_length (math "40-$user_length-1-$host_length-1")
+		set user_host_length (math "$user_length+1+$host_length+1")
+		if [ $user_host_length -ge $default_length ];
+			set should_show_path false
+		end
+		set total_length (math "$default_length-$user_host_length")
 		if [ $working_path_length -gt $total_length ];
 			set total_length (math "$total_length-3")
 			set path_prefix "..."
 		end
 	end
 	set working_path_sub (echo -n $working_path | tail -c $total_length)
+	if [ $should_show_path = false ];
+		set working_path_sub ""
+	end
 	echo -n $path_prefix$working_path_sub
 end
 
