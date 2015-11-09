ebceac3880eab5c5b783d0a42dd8fa0bc7e63e1d
Samantha Marshall <me@samdmarshall.com>
2015-11-09 11:34:13 -0500

    💻

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -102,45 +102,20 @@ end
 
 function svn_prompt
 	set svn_revision (svn info $pwd | grep "Last Changed Rev: " | sed -e "s=Last Changed Rev: ==" -e "s=\n==g")
-	printf ' (%s%s%s'  (set_color $__fish_git_prompt_color_branch) $svn_revision (set_color normal) 
+	printf '(%s%s%s'  (set_color $__fish_git_prompt_color_branch) $svn_revision (set_color normal) 
 	set svn_status_lines (svn stat | sed -e 's=^Summary of conflicts.*==' -e 's=^  Text conflicts.*==' -e 's=^  Tree conflicts.*==' -e 's=.*incoming .* upon update.*==' | cut -c 1-7 | tr '\n' ':')
-	set found_first_column 0
-	set previous_nonempty_column 0
 	set status_state ""
 	for col in (seq 7)
-		set svn_status (echo $svn_status_lines | tr ':' '\n' | cut -c $col | sort | uniq | tr -d '\n' | tr -d ' ')
-
-		if [ "$svn_status" != "" ];
-			if [ $found_first_column -eq 0 ];
-				set found_first_column $col
-				set status_state $status_state'|'
-			end
-		end
-
-		if [ $found_first_column != 0 ];
-			if [ "$svn_status" != "" ];
-				set previous_nonempty_column $col
-			end
-
-			if [ $previous_nonempty_column != $found_first_column ];
-				set status_state $status_state'|'
-			end
-
-			if [ "$svn_status" != "" ];
-				set tags (parse_svn_status $svn_status)
-				set status_state $status_state$tags
-			end
-		else
-			set status_state $status_state'|'
-		end
+		set svn_status (echo $svn_status_lines | tr ':' '\n' | cut -c $col | sort | uniq | tr -d '\n')
+		set status_state $status_state $svn_status
 	end
-	set was_not_empty false
-	set status_length (echo $status_state | tr -d '|' | numchar)
-	if [ $status_length -gt 1 ];
-		set was_not_empty true
-	end
-	if [ $was_not_empty = true ];
-		printf $status_state
+	for state in $status_state
+		set state_string (echo -n "$state" | tr -d ' ')
+		set state_string_length (echo -n "$state_string" | numchar)
+		if [ $state_string_length -ge 1 ];
+			set tags (parse_svn_status $state_string)
+			printf '|%s' $tags
+		end
 	end
 	printf ')'
 end
