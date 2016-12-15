88874b369b07125e40180e33d192dbca054fc54a
Samantha Marshall <me@samdmarshall.com>
2016-12-15 15:00:45 -0500

    updating prompt to speed it up and not cause the errors in other ssh sessions

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -1,38 +1,9 @@
-# fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-
-# Status Chars
-set __fish_git_prompt_char_dirtystate '↯'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
-
 # setting up colours
-if status --is-login
-    if not set -q __fish_prompt_user
-        set -g __fish_prompt_user (set_color -o d43582)
-    end
-    if not set -q __fish_prompt_host
-        set -g __fish_prompt_host (set_color b58900)
-    end
-    if not set -q __fish_prompt_path
-        set -g __fish_prompt_path (set_color 299e95)
-    end
-    if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
-    end
-end
-
-if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname -s)
-end
+set __fish_prompt_user -o d43582
+set __fish_prompt_host b58900
+set __fish_prompt_path 299e95
+set __fish_prompt_normal normal
+set __fish_prompt_hostname (hostname -s)
 
 function fish_right_prompt
     if test (defaults read com.pewpewthespells.notmuch-notifier hasmail) -ne 0
@@ -44,17 +15,17 @@ function fish_prompt
     if set -q SSH_CONNECTION
         printf 'Connected to -> %s\n' $__fish_prompt_hostname
     end
-    printf '%s' $__fish_prompt_user
+    printf '%s' (set_color $__fish_prompt_user)
     printf '%s' $USER
-    printf '%s' $__fish_prompt_normal
+    printf '%s' (set_color $__fish_prompt_normal)
     printf ': '
 end
 
 function print_segment --argument colour --argument string_value
     printf '<‌'
-    printf '%s' $colour
+    printf '%s' (set_color $colour)
     printf '%s' $string_value
-    printf '%s' $__fish_prompt_normal
+    printf '%s' (set_color $__fish_prompt_normal)
     printf '‌>'
 end
 
