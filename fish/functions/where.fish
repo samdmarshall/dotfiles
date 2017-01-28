4c6ebff2a38b5bc92fd544fec25f7c305ab2ed2f
Samantha Marshall <me@samdmarshall.com>
2017-01-28 10:24:41 -0500

    making use of the full path

--- fish/functions/where.fish
+++ fish/functions/where.fish
@@ -4,7 +4,7 @@ end
 
 function where --description 'display the current host and working path'
     set -l where_host (print_segment $__fish_prompt_host $__fish_prompt_hostname)
-    set -l where_path (print_segment $__fish_prompt_path (prompt_pwd))
+    set -l where_path (print_segment $__fish_prompt_path $PWD)
     set -l where_modifier "in"
     if set -q SSH_CONNECTION
         set where_modifier "via ssh; in"
