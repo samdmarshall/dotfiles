3e251d070072fb0577c8dabda54d8bc8aa48481b
Samantha Marshall <me@samdmarshall.com>
2017-04-01 18:45:52 -0700

    handling string escapes correctly

--- fish/wrappers/hub.fish
+++ fish/wrappers/hub.fish
@@ -2,6 +2,6 @@ switch (command echo "$FISH_PLATFORM_NAME")
     case 'Darwin' 'darwin'
         complete --command hub --wraps=git
         function hub --wraps=git
-            grimoire hub $argv
+            grimoire hub \"$argv\"
         end
 end
