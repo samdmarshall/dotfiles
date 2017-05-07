816ac3456e87db0dc1cfc5fb0d8505373fa92f19
Samantha Marshall <me@samdmarshall.com>
2017-05-07 09:45:22 -0400

    Updating config and adding new command wrapper
    * Updating the script to tag emails when they come in so that i'm getting notifications about my NAS sorted correctly
    * Adding convenience wrapper around `pwgen` to make it more usable

---
+++ fish/wrappers/pwgen.fish
@@ -0,0 +1,6 @@
+switch (command echo "$FISH_PLATFORM_NAME")
+    case 'Darwin' 'darwin'
+        function pwgen --wraps=pwgen
+            command pwgen -s 32 1 $argv | tr -d '\n' -d ' ' | pbcopy
+        end
+end
