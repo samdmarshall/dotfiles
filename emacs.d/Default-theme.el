8beb4cad6cbd447f9e19bd8bf890fc190d002bc6
Samantha Demi <me@samdmarshall.com>
2019-01-25 21:54:33 -0500

    changing color themes and adjusting programs to match. also adding battery display to WSL env

--- emacs.d/Default-theme.el
+++ emacs.d/Default-theme.el
@@ -3,7 +3,7 @@
 
 (custom-theme-set-variables
  'Default
- '(package-selected-packages (quote (fish-mode ergoemacs-mode solarized-theme nim-mode nlinum on-screen rainbow-mode vdiff vlf spinner)))
+ '(package-selected-packages (quote (fish-mode ergoemacs-mode nim-mode nlinum on-screen rainbow-mode vdiff vlf spinner)))
  '(standard-indent 2)
  '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
 
--- fish/prompt.fish
+++ fish/prompt.fish
@@ -5,11 +5,28 @@ set __fish_prompt_path 299e95
 set __fish_prompt_normal normal
 set __fish_prompt_hostname (hostname -s)
 
+
+function display_battery_level
+  set -l level (battery_level --default)
+  set -l battery_color brgreen
+  if test $level -lt 10
+    set battery_color brred
+  else if test $level -lt 25
+    set battery_color red
+  else if test $level -lt 50
+    set battery_color yellow
+  else if test $level -lt 75
+    set battery_color green
+  end
+  printf '[%s%s%%%s]' (set_color $battery_color) $level (set_color $__fish_prompt_normal)
+end
+
 function fish_right_prompt
 	command --search coven > /dev/null
 	if test $status -eq 0
-		printf '%s' (coven)
+		printf '%s ' (coven)
 	end
+	display_battery_level
 end
 
 function fish_prompt
