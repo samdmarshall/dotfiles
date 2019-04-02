1cb18f0ace2fb9fecc2af4d977ae6c73dcf75d32
Samantha Demi <hello@pewpewthespells.com>
2019-04-01 22:42:23 -0400

    consolidate shell prompt into my utilities

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -6,33 +6,12 @@ set __fish_prompt_normal normal
 set __fish_prompt_hostname (hostname -s)
 
 
-function display_battery_level
-  set -l level (battery_level --default)
-  set -l battery_color brgreen
-  if test $level -lt 10
-    set battery_color brred
-  else if test $level -lt 25
-    set battery_color red
-  else if test $level -lt 50
-    set battery_color yellow
-  else if test $level -lt 75
-    set battery_color green
-  end
-  if test (battery_level --default --charging) -eq 1
-    set battery_color $battery_color --bold
-  end
-  printf '[%s%s%%%s]' (set_color $battery_color) $level (set_color $__fish_prompt_normal)
-end
-
 function fish_right_prompt
-	if test -x (command --search coven)
-		printf '%s ' (coven)
-	else
-		printf '⛔ '
-	end
-	display_battery_level
+  printf '%s %s' (coven) (batterylevel --default --color --status)
 end
 
 function fish_prompt
+  printf '\n%s\n' (when)
+  printf '%s\n' (where --color=always) 
   printf '(%s%s%s) ' (set_color $__fish_prompt_user) $USER (set_color $__fish_prompt_normal)
 end
